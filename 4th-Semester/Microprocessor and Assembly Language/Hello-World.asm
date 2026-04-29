a2ec523b9416fa8464a8b14d9a7fc4d4c9829ad8
Mehedi <mehedi.hasan.tsx@gmail.com>
2026-04-30 00:26:58 +0600

    Add assembly programs to print "Hello World"

---
+++ 4th-Semester/Microprocessor and Assembly Language/Hello-World.asm
@@ -0,0 +1,18 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG DB 'Hello World$'
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    LEA DX, MSG
+    MOV AH, 09H
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
